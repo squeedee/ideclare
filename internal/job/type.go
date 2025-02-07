549b6e803cb08a12305019affeb19ed4814e6bf6
Rasheed Abdul-Aziz <squeedee@gmail.com>
2025-02-06 19:06:34 -0500

    Wip: Working out mapping

--- internal/job/type.go
+++ internal/job/type.go
@@ -4,19 +4,35 @@ package job
 // In my mind, this is a vital lift. Job author says "I need this input, and I can trust that the system will capture its signature"
 // Maybe the difference between "Env Vars" and these "digestable inputs" is that they are "Externals".
 
-const Digestable_Config = "config"
-const Digestable_Input = "input"
+const Digestable_Value = "Value"
+const Digestable_OciRegistry = "OCIRegistry"
+const Digestable_UrlBody = "URLBody"
 
-type EnvVar struct {
-	Name   string `json:"name"`
-	Desc   string `json:"description"`
-	Type   string `json:"type"` // Validate enum, default to config
-	Digest string `json:"digest"`
+type Mapping struct {
 }
 
-type Env []EnvVar
+type Input struct {
+	// Name the input. This is also used as the default mapping to Environment variables, see Mapping.
+	Name string `json:"name"`
+
+	// Description is a free text definition of the input.
+	Description string `json:"description"`
+
+	// Type is a free text definition
+	Type string `json:"type"`
+
+	// Optional is true if this value does not need to be provided
+	Optional bool `json:"optional"`
+
+	// Default
+	Default string `json:"default"`
+
+	// Mapping defines how the input is passed into the container
+	Mapping Mapping `json:"mapping"`
+}
+
+type Inputs []Input
 
 type Spec struct {
-	// Env records all the environment variables
-	Env Env `json:"env"`
+	Inputs Inputs `json:"env"`
 }
